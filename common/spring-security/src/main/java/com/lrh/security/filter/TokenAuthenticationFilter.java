955efbfc495a22ea7061133ca4634ea6823e7eb7
Cookici <632832232@qq.com>
2023-05-05 16:25:26 +0800

    SpringSecurity实现用户角色菜单授权

--- common/spring-security/src/main/java/com/lrh/security/filter/TokenAuthenticationFilter.java
+++ common/spring-security/src/main/java/com/lrh/security/filter/TokenAuthenticationFilter.java
@@ -11,14 +11,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
